85c4d8b009a8dd90d9e9844fda0d1d71a6ca7869
mohammad afsar <35555773+afsarahmad786@users.noreply.github.com>
2022-10-03 20:48:12 +0530

    Fles Added

---
+++ app.js
@@ -0,0 +1,7 @@
+document.title = 123;
+var headerTitle = document.getElementById("header-title");
+headerTitle.style.borderBottom = "solid 3px #000";
+
+var item = document.getElementById("itemss");
+item.style.fontWeight = "bold";
+item.style.backgroundColor = "green";
