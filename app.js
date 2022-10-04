da2698c5dea26daaec6e792e6b962fd8d88a8dee
mohammad afsar <35555773+afsarahmad786@users.noreply.github.com>
2022-10-04 07:44:54 +0530

    Added Li Tag and Tagname Property

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ var item = document.getElementById("itemss");
 item.style.fontWeight = "bold";
 item.style.backgroundColor = "green";
 
-var it = document.getElementsByClassName("list-group-item");
-it[1].style.fontWeight = "bold";
-it[0].style.fontWeight = "bold";
-it[2].style.fontWeight = "bold";
-it[3].style.fontWeight = "bold";
+// var it = document.getElementsByClassName("list-group-item");
+// it[1].style.fontWeight = "bold";
+// it[0].style.fontWeight = "bold";
+// it[2].style.fontWeight = "bold";
+// it[3].style.fontWeight = "bold";
+
+var li = document.getElementsByTagName("li");
+li[1].style.fontWeight = "bold";
+li[0].style.fontWeight = "bold";
+li[2].style.fontWeight = "bold";
+li[3].style.fontWeight = "bold";
+
+for (var i = 0; i < li.length; i++) {
+  li[i].style.backgroundColor = "#f4f4f4";
+}
