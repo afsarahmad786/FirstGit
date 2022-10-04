a40b29c2341eca87b02ee1c0a4b2a3b82c4f692b
mohammad afsar <35555773+afsarahmad786@users.noreply.github.com>
2022-10-04 07:39:06 +0530

    Added Bold Font to All List Item

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ headerTitle.style.borderBottom = "solid 3px #000";
 var item = document.getElementById("itemss");
 item.style.fontWeight = "bold";
 item.style.backgroundColor = "green";
+
+var it = document.getElementsByClassName("list-group-item");
+it[1].style.fontWeight = "bold";
+it[0].style.fontWeight = "bold";
+it[2].style.fontWeight = "bold";
+it[3].style.fontWeight = "bold";
