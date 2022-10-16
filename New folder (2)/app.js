001f613c945841cc0b0bd33580d4c5c40c480e10
mohammad afsar <35555773+afsarahmad786@users.noreply.github.com>
2022-10-16 09:37:21 +0530

    call apply and bind

--- New folder (2)/app.js
+++ New folder (2)/app.js
@@ -1,1 +1,81 @@
-console.log("I Love Git");
+An 1:
+
+var obj={num:2};
+
+
+var add=function(a){
+
+    return this.num+a;
+
+}
+
+
+console.log(add.call(obj,3));
+
+
+Ans 2:
+
+var obj={num:2};
+
+
+var add=function(a,b,c){
+
+    return this.num+a+b+c;
+
+}
+
+
+
+var arr=[1,2,3];
+
+console.log(add.apply(obj,arr));
+
+
+
+Ans 3:
+
+var obj={num:2};
+
+
+var add=function(a,b,c){
+
+   return this.num+a+b+c;
+
+}
+
+
+var arr=[1,2,3];
+
+var bound=add.bind(obj);
+
+console.log(bound(1,2,3));
+
+
+
+Ans4:
+
+class Student {
+
+ constructor(age) {
+
+   this.age=age
+
+ }
+
+
+ static age(t) {
+
+   return this.age+t;
+
+ }
+
+
+}
+
+
+const obj = new Student(10);
+
+
+var ages=Student.age.bind(obj)
+
+console.log(ages(0))
